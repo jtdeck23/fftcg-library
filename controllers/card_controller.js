caeaa98d1be6ab42f0042c88564e42b3cf1b16ea
jtdeck23 <jordandeck060@gmail.com>
2018-12-25 18:58:50 -0600

    implemented fuzzy search on card library

--- controllers/card_controller.js
+++ controllers/card_controller.js
@@ -6,14 +6,27 @@ module.exports = {
     },
 
     library(req, res, next) {
-        Card.find({}, (err, cards) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render('library.hbs', {cards: cards});
-                console.log(cards);
-            }
-        });
+        var escapeRegex = (text) => {
+            return text.replace(/[-[\]{}()*+?.,\\^$!#\s]/g, "\\$&");
+        };
+        if (req.query.name) {
+            const regex = new RegExp(escapeRegex(req.query.name), 'gi');
+            Card.find({name: regex}, (err, cards) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render('library.hbs', {cards: cards});
+                }
+            });
+        } else {
+            Card.find({}, (err, cards) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render('library.hbs', {cards: cards});
+                }
+            });
+        }      
     },
 
     newCard(req, res, next) {
